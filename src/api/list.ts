5392076241ac2eeafc2cf2953bc1366ee068e337
Grant Timmerman <timmerman+devrel@google.com>
2019-01-07 11:55:41 -0800

    Show total number of repos when mgit list

--- src/api/list.ts
+++ src/api/list.ts
@@ -5,6 +5,14 @@ import { spinner } from './../utils';
 // Page page size with GitHub API
 const MAX_PAGE_SIZE = 100;
 
+// Gets the number of repos (public + private) by a user.
+const getNumRepos = async (username: string) => {
+  const user = await octokit.orgs.get({
+    org: username,
+  });
+  return user.data.public_repos + user.data.owned_private_repos;
+};
+
 /**
  * Lists the user/repo names.
  * @param {string} username The GitHub username or org name.
@@ -15,11 +23,13 @@ export async function apilist(username: string): Promise<any> {
   let page = 1;
   let listIsEmpty = false;
 
+  const totalRepos = await getNumRepos(username);
+
   // Gets a list of repos as a specific page.
   const getList: (page: number) => Promise<Response<any>> = async (page: number) => {
     const rangeStart = (page - 1) * MAX_PAGE_SIZE;
     const rangeEnd = page * MAX_PAGE_SIZE;
-    const rangeString = `Getting repos ${rangeStart}-${rangeEnd}...`;
+    const rangeString = `Getting repos ${rangeStart}-${rangeEnd}/${totalRepos}...`;
     spinner.setSpinnerTitle(rangeString);
     return await octokit.repos.listForUser({
       username,
